4e445899397ba686d03ba0b809890dfa74093bbd
Dewald <dewaldfourie0808@gmail.com>
2024-04-09 08:41:45 +0200

    updated get route functionality

--- app.js
+++ app.js
@@ -1,29 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const session = require("express-session");
+const MongoStore = require('connect-mongo');
 
-// package documentation - https://www.npmjs.com/package/connect-mongo
-const MongoStore = require('connect-mongo')(session);
+const app = express();
 
-// dewaldfourie0808
-// phBznJoGbwe7LE95
-
-// mongodb+srv://dewaldfourie0808:phBznJoGbwe7LE95@cluster0.jiegnyo.mongodb.net/?retryWrites=true&w=majority
-
-// Create the Express App
-var app = express();
-// <user>:<password>@
 const dbString = 'mongodb+srv://dewaldfourie0808:phBznJoGbwe7LE95@cluster0.jiegnyo.mongodb.net/?retryWrites=true&w=majority';
 const dbOptions = {
-    userNewUrlParser: true,
+    useNewUrlParser: true,
     useUnifiedTopology: true,
 }
+
 const connection = mongoose.createConnection(dbString, dbOptions);
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 
 const sessionStore = new MongoStore({
+    mongoUrl: dbString,
     mongooseConnection: connection,
     collection: 'sessions',
 });
@@ -34,12 +28,21 @@ app.use(session({
     saveUninitialized: true,
     store: sessionStore,
     cookie: {
-        maxAge: 1000 * 60 * 60 * 24 // equals 1 day ( 1000ms * 60s * 60min * 24hr)
+        maxAge: 1000 * 60 * 60 * 24 // equals 1 day (1000ms * 60s * 60min * 24hr)
     }
 }));
 
-app.get('/', (req, res. next) => {
-    res.send(`<h1>Hello World (sessions)</h1>`);
+app.get('/', (req, res, next) => {
+    
+    if (req.session.viewCount) {
+        req.session.viewCount = req.session.viewCount + 1;
+    } else {
+        req.session.viewCount = 1;
+    }
+
+    res.send(`<h1>You have visited this page ${req.session.viewCount} times</h1>`);
 });
 
-app.listen(3000);
+app.listen(3000, () => {
+    console.log("Server is running on port 3000");
+});
